06b79be3fc3fe58dce56fa2c9d6ba0afa953a2ad
yhuzued <yusufmait@gmail.com>
2022-11-13 14:45:20 +0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  # the root isnt going anywhere
+  root "pages#home"
+end
