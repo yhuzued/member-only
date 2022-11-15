0c361bc70e2ab752fcb583ddc605065dfd850271
yhuzued <yusufmait@gmail.com>
2022-11-15 14:25:10 +0700

    Styling post and index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true
 
   belongs_to :user
